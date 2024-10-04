8f383f3b975346b015bdefe0e80a854d74989cd2
KevinEswein <kevin.eswein@singular-it.de>
2024-10-04 13:11:10 +0200

    add animation

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
-function numbers() {
+const img = document.getElementById('rotating-disc');
+const frames = 24; // Anzahl der Einzelbilder (0°, 15°, ..., 345°)
+let currentFrame = 0;
+let isAnimating = false;
+let animationInterval;
 
-  document.write("Numbers: ");
-
-  for (var i = 0; i < 1000; i++) {
-    document.write(i + " ");
-  }
+function updateImage() {
+  img.src = `data/disc_${currentFrame * 15}.png`;
 }
+
+function rotateLeft() {
+  currentFrame = (currentFrame - 1 + frames) % frames;
+  updateImage();
+}
+
+function rotateRight() {
+  currentFrame = (currentFrame + 1) % frames;
+  updateImage();
+}
+
+function toggleAnimation() {
+  if (isAnimating) {
+    clearInterval(animationInterval);
+  } else {
+    animationInterval = setInterval(rotateRight, 100);
+  }
+  isAnimating = !isAnimating;
+}
+
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'L' || event.key === 'l') {
+    rotateLeft();
+  } else if (event.key === 'R' || event.key === 'r') {
+    rotateRight();
+  } else if (event.key === 'A' || event.key === 'a') {
+    toggleAnimation();
+  }
+});
